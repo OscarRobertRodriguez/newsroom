05081484fc1bb4a00f708ff052ccd796da9ee781
Phil <phillosty@hotmail.com>
2018-01-20 14:05:00 +0000

    Update the footer so the subscription form uses a lot of the same styling and html as the BOV academy (which this widget is based on). There is no corresponding javascript in place though to handle the animation / submtiing details

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,5 @@
 import 'normalize.css';
 import '../styles/main.scss';
 import '../assets/images/meTest.jpg';
-import '../assets/images/footerSubscriptionLogo.png';
 
 import './scripts/examples/examples';
