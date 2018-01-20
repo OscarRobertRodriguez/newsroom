e7f2422c7b3d61452eafa75c0cdac694a841e0a0
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2018-01-20 13:22:11 -0600

    replaced icons to main dir

--- src/app/index.js
+++ src/app/index.js
@@ -3,5 +3,10 @@ import 'normalize.css';
 import '../styles/main.scss';
 import '../assets/images/meTest.jpg';
 import '../assets/images/code.jpg';
+import '../assets/images/icons/3d-icon.png';
+import '../assets/images/icons/chatBubble-icon.png'; 
+import '../assets/images/icons/contentCopy-icon.png';
+import '../assets/images/icons/expand-icon.png'; 
+import '../assets/images/icons/vision-icon.png';
 
 import './scripts/examples/examples';
