5b249ebf4c1d904edc9bcb81a1a92ec6b9580300
Phil <phillosty@hotmail.com>
2018-01-20 10:44:22 +0000

    updating the subscription field

--- src/app/index.js
+++ src/app/index.js
@@ -2,5 +2,6 @@
 import 'normalize.css';
 import '../styles/main.scss';
 import '../assets/images/meTest.jpg';
+import '../assets/images/footerSubscriptionLogo.png';
 
 import './scripts/examples/examples';
