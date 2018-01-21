a8e27a257cba800789b2483b92c4e6d6ddd44271
Vojislav Grujic <vojislav.grujic@gmail.com>
2018-01-21 21:07:34 +0100

    Updated outline button to match the design. Fix formatting inconsistencies

--- src/app/scripts/mediaObjectTwitter.js
+++ src/app/scripts/mediaObjectTwitter.js
@@ -1,11 +1,10 @@
 import twitterLogoBlue from '../../assets/images/Twitter_Logo_Blue.svg';
 import twitterLogoRed from '../../assets/images/Twitter_Logo_Red.svg';
 
-
 const twitterLogo = document.querySelector('.twitterLogo');
 const twitterFollowBtns = Array.from(document.querySelectorAll('.twitterFollowBtn'));
 
 twitterLogo.src = twitterLogoBlue;
-twitterFollowBtns.forEach( (btn) => {
+twitterFollowBtns.forEach((btn) => {
   btn.src = twitterLogoRed;
 });
