94d60e625f8ecf125d2c3a3a69e4ed33948f9a9d
Joshua Sadlon <jjs457@gmail.com>
2018-01-21 20:16:27 -0500

    added style changes and icons to header/footer (#38)
    * added style changes and icons to header/footer
    
    * added border to top of header, added padding in header as well

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,7 @@ import '../styles/main.scss';
 import '../assets/images/meTest.jpg';
 import '../assets/images/code.jpg';
 import '../assets/images/logoBovAcademy.png'; 
+import '../assets/images/dev_news@3x.png';
 
 // JS components
 // import './scripts/examples/examples';
