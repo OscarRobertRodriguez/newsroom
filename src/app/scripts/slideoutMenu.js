e5e8cb2e2b00d74f15fe8e681c57c2c6e16c68c9
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-01-24 13:40:39 +1100

    Resolved slideout menu's close window and click issues

--- src/app/scripts/slideoutMenu.js
+++ src/app/scripts/slideoutMenu.js
@@ -2,31 +2,48 @@
 const slideoutWhatshotIcon = document.querySelector("#slideout-menu-icon-whatshot");
 const slideoutBookmarkIcon = document.querySelector("#slideout-menu-icon-bookmark");
 const slideoutNotificationIcon = document.querySelector("#slideout-menu-icon-notification");
+const slideoutCloseBtns = document.querySelectorAll(".slideout-menu__btn-close");
+const colorWhatshotIcon = `#39c3d9`;
+const colorBookmarkIcon = `#7fe8a4`;
+const colorNotificationIcon = `#ff9600`;
 
 slideoutWhatshotIcon.addEventListener("click", function(){
-  this.checked ?  slideIconsToLeft(slideoutBookmarkIcon, slideoutNotificationIcon) : slideIconsToRight(slideoutBookmarkIcon, slideoutNotificationIcon);
+  if(this.checked) {
+    slideIconsToLeft(this, slideoutBookmarkIcon, colorBookmarkIcon, slideoutNotificationIcon, colorNotificationIcon);
+  }
 });
 slideoutBookmarkIcon.addEventListener("click", function(){
-  this.checked ?  slideIconsToLeft(slideoutWhatshotIcon, slideoutNotificationIcon) : slideIconsToRight(slideoutWhatshotIcon, slideoutNotificationIcon);
+  if(this.checked) {
+    slideIconsToLeft(this, slideoutWhatshotIcon, colorWhatshotIcon, slideoutNotificationIcon, colorNotificationIcon);
+  }
 });
 slideoutNotificationIcon.addEventListener("click", function(){
-  this.checked ?  slideIconsToLeft(slideoutWhatshotIcon, slideoutBookmarkIcon) : slideIconsToRight(slideoutWhatshotIcon, slideoutBookmarkIcon);
+  if(this.checked) {
+    slideIconsToLeft(this, slideoutWhatshotIcon, colorWhatshotIcon, slideoutBookmarkIcon, colorBookmarkIcon);
+  }
+});
+
+slideoutCloseBtns.forEach((closeBtn) => {
+  closeBtn.addEventListener("click", slideoutCloseWindow);
 });
 
 // Function to slide icons to the left
-function slideIconsToLeft(icon1, icon2){
-  icon1.setAttribute("disabled", true);
-  icon1.nextElementSibling.style.right = "329px";
-  icon2.setAttribute("disabled", true);
-  icon2.nextElementSibling.style.right = "329px";
+function slideIconsToLeft(ele, icon1, icon1Bg, icon2, icon2Bg){
+  ele.nextElementSibling.style.cssText = `background: white; border-right: 1px solid white; right: 320px; width: 47px;`
+  ele.nextElementSibling.nextElementSibling.removeAttribute(`style`);
+  icon1.nextElementSibling.style.cssText = `background: ${icon1Bg}; color: white; right: 329px; width: 36px;`
+  icon2.nextElementSibling.style.cssText = `background: ${icon2Bg}; color: white; right: 329px; width: 36px;`
 }
 
-// Function to slide icons back to the right
-function slideIconsToRight(icon1, icon2){
-  icon1.removeAttribute("disabled");
-  icon1.nextElementSibling.removeAttribute("style");
-  icon2.removeAttribute("disabled");
-  icon2.nextElementSibling.removeAttribute("style");
+// Function to close the window
+function slideoutCloseWindow(){
+  const self = this;
+  const slideoutCloseBtnParent = self.parentElement;
+  slideoutCloseBtnParent.style.right = "-322px";
+  slideoutWhatshotIcon.checked = false;
+  slideoutBookmarkIcon.nextElementSibling.style.cssText = `right: 0; background: ${colorWhatshotIcon}, color: white `;
+  slideoutBookmarkIcon.checked = false;
+  slideoutWhatshotIcon.nextElementSibling.style.cssText = `right: 0; background: ${colorBookmarkIcon}, color: white `;
+  slideoutNotificationIcon.checked = false;
+  slideoutNotificationIcon.nextElementSibling.style.cssText = `right: 0; background: ${colorNotificationIcon}, color: white `;
 }
-
-
