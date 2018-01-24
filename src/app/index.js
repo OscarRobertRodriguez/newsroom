21212288e85020ebbe49b3f4b7a7f94c6ce0aa3c
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-01-24 14:08:01 +1100

    Resolved conflict issues in index.js

--- src/app/index.js
+++ src/app/index.js
@@ -5,12 +5,7 @@ import '../assets/images/meTest.jpg';
 import '../assets/images/code.jpg';
 import '../assets/images/logoBovAcademy.png'; 
 import '../assets/images/dev_news@3x.png';
-
-
-import mediaOjectTwitter from './scripts/mediaObjectTwitter'
 import slideoutMenu from './scripts/slideoutMenu'
-
-import './scripts/examples/examples';
 
 // JS components
 // import './scripts/examples/examples';
@@ -18,8 +13,4 @@ import './scripts/examples/toggleModals'; // TEMPORARY
 import './scripts/categorySwitcher';
 import './scripts/dateObject';
 import './scripts/mediaObjectTwitter';
-
-
-
 import './scripts/scrollspy';
-
