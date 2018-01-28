b278c57aeeaf8e25fe5040f9a8ece956fdd59186
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2018-01-28 17:50:47 -0600

    modified index html and js to updated with webpack modules

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,15 @@
 // import main scss file and reset 
-import 'normalize.css';
+import 'normalize.scss/normalize.scss';
 import '../styles/main.scss';
-import '../assets/images/meTest.jpg';
-import '../assets/images/code.jpg';
-import '../assets/images/logoBovAcademy.png'; 
-import '../assets/images/dev_news@3x.png';
-import '../assets/images/dev_video_1.jpeg';
-import '../assets/images/dev_video_2.jpg';
-import '../assets/images/dev_video_3.png';
-import slideoutMenu from './scripts/slideoutMenu'
+
+
+import '../index.html';
+
 
 // JS components
 // import './scripts/examples/examples';
+import './scripts/slideoutMenu';
 import './scripts/examples/toggleModals'; // TEMPORARY
 import './scripts/categorySwitcher';
 import './scripts/dateObject';
-import './scripts/mediaObjectTwitter';
 import './scripts/scrollspy';
